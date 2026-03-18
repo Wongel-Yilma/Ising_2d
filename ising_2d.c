f7621cbe8211cdd224ef0c82d716594b3242c849
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-18 14:27:04 -0600

    Added enrgy and magntization calculating funcitons

--- ising_2d.c
+++ ising_2d.c
@@ -4,6 +4,9 @@
 
 void Initialize(int , int**);
 void MC_move(int ,int**, double);
+void Calculate_energy(int, int**, double *);
+void Calculate_magnetization(int, int**, double *);
+
 
 int main(){
     int N =4;
@@ -16,25 +19,32 @@ int main(){
     // Declare the neccessary variablesto run the sim
     int nsteps = 10;
     double T = 1.5;
-    MC_move(N,  config, T);
-
-
+    double total_energy;
+    double magnetization;
+    int step;
+    for (step=0; step<nsteps; step++){
+        MC_move(N,  config, T);
+        Calculate_energy(N,config, &total_energy);
+        Calculate_magnetization(N,config, &magnetization);
+        printf("Step: %d    Energy: %f    Mag: %f     \n", step, total_energy, magnetization);
+    }
 
     return 0;
 }
 
 void Initialize(int N, int**config){
     int i,j;
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
-            config[i][j] = (int)( 2*((double)rand()/RAND_MAX)-1);
+    for ( i=0; i<N; i++){
+        for ( j=0; j<N; j++){
+            config[i][j] = (rand() % 2 == 0) ? -1 : 1;
+            printf("x= %d y= %d: spin=%d ", i, j, config[i][j]);
         }
     }
 }
 
 void MC_move(int N, int**config, double beta){
     int i, j, spin, rx, ry, neigh_sum, cost;
-    
+
 
     for (i=0; i<N; i++){
         for (j=0; j<N; j++){
@@ -51,5 +61,28 @@ void MC_move(int N, int**config, double beta){
             }
             config[rx][ry]=spin;
         }   
+    }
+}
+
+void Calculate_energy(int N, int** config , double* total_energy){
+    int i, j, spin, neigh_sum;
+    *total_energy=0.0;
+    for ( i=0; i<N; i++){
+        for ( j=0; j<N; j++){
+            spin = config[i][j];
+            neigh_sum = config[(i-1+N)%N][j] + config[(i+1)%N][j] + config[i][(j-1+N)%N] + config[i][(j+1)%N];
+            *total_energy+= -neigh_sum*spin;
+        }
+    }
+    *total_energy/=4;
+}
+
+void Calculate_magnetization(int N, int** config , double* magnetization){
+    int i, j, spin, neigh_sum;
+    *magnetization=0.0;
+    for ( i=0; i<N; i++){
+        for ( j=0; j<N; j++){
+            *magnetization+=config[i][j];
+        }
     }
 }
