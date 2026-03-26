f6a3624cabfac0b0e6de0a0cb8991eb7f9f20796
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-26 11:53:21 -0600

    Corrected indexing and temperature scale

--- ising_2d.cpp
+++ ising_2d.cpp
@@ -9,7 +9,7 @@ class Ising
 {
 public:
     int N;
-    double beta;  // Beta = kB*T  -> Boltzmann's temperature
+    double temp;  
     double total_energy;
     double magnetization;
     bool first_dump_write=true;
@@ -20,9 +20,9 @@ public:
     std::ofstream fp;
     std::string columns ="id type x y z s";
     // Class constructor
-    Ising(int lattice_size, int boltzmann_temp){
+    Ising(int lattice_size, double boltzmann_temp){
         N = lattice_size;
-        beta = boltzmann_temp;
+        temp = boltzmann_temp;
         config  = new int[N*N];
     }
     ~Ising (){
@@ -43,44 +43,48 @@ public:
         }
     }
     void MC_Move(){ 
-        int row, col, spin, rx, ry, neigh_sum, cost;
+        int row, col, spin, ry, rx, neigh_sum, cost;
         for (row=0; row<N; row++){
             for (col=0; col<N; col++){
-                rx = rand()%N;
                 ry=  rand()%N;
-                spin = config[rx*N+ry];
-                neigh_sum = config[((rx-1+N)%N)*N+ry] + config[N*((rx+1)%N)+ry] + config[rx*N+(ry-1+N)%N] + config[rx*N+(ry+1)%N];
+                rx = rand()%N;
+                spin = config[ry*N+rx];
+                neigh_sum = config[((ry-1+N)%N)*N+rx] + config[N*((ry+1)%N)+rx] + config[ry*N+(rx-1+N)%N] + config[ry*N+(rx+1)%N];
+                double rn = rand()/(double)RAND_MAX;
                 cost = 2*spin*neigh_sum;
                 if (cost<0){
                     spin*=-1;
                 }
-                else if ((rand()/RAND_MAX)< exp(-cost*beta)){
+                else if (rn< exp(-cost/temp)){
                     spin*=-1;
                 }
-                config[rx*N+ry]=spin;
+                config[ry*N+rx]=spin;
             }   
         }
         step++;
     }
-    void Calculate_energy(){
+    double Calculate_energy(){
         int row, col, spin, neigh_sum;
         total_energy=0.0;   
         for ( row=0; row<N; row++){
             for ( col=0; col<N; col++){
                 spin = config[row*N+col];
-                neigh_sum = config[N*((row-1+N)%N)+col] + config[(row+1)%N+col] + config[row*N+(col-1+N)%N] + config[row*N + (col+1)%N];
+                neigh_sum = config[N*((row-1+N)%N)+col] + config[N*((row+1)%N)+col] + config[row*N+(col-1+N)%N] + config[row*N + (col+1)%N];
                 total_energy+= -neigh_sum*spin;
             }
         }
         total_energy/=4;
+        return total_energy;
     }
-    void Calculate_magnetization(){
+    double Calculate_magnetization(){
         int i;
         magnetization=0.0;
         for ( i=0; i<N*N; i++){
             magnetization+=config[i];
         }
+        return magnetization;
     }
+
     void Dump(){
         if (first_dump_write){
             fp.open(file_name, std::ios::out);
@@ -107,6 +111,7 @@ public:
         }
         fp.close();
     }
+
 };
 
 
@@ -114,13 +119,20 @@ public:
 
 int main(int argn, char* argv[]){
     // std::string input_file = argv[1];
-    Ising*  ising_sim = new Ising(100, 0.00259);
-    int nsteps = 1000;
+    srand(100);
+    double energy, mag;
+    Ising*  ising_sim = new Ising(400, 2.5);
+    int nsteps = 40000;
     ising_sim->Initialize();
-    ising_sim->Dump();
+    ising_sim->Dump(); 
     for (int step=0; step<nsteps; step++){
         ising_sim->MC_Move();
-        ising_sim->Dump();
+        if (step%500==0) {
+            ising_sim->Dump();
+            energy = ising_sim->Calculate_energy();
+            mag = ising_sim->Calculate_magnetization();
+            printf("Step: %d ; Energy: %f ; Mag: %f \n", step, energy, mag);
+        }
     }
     return 0;
 }
