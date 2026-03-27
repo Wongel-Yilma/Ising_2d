3e3f95c1d474c7d1875210ba01d3b5fa0f2e8b9d
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-03-27 17:00:24 -0600

    Changed random number generator to mersenne twister engine

--- ising_2d.cpp
+++ ising_2d.cpp
@@ -10,9 +10,12 @@
 #include <algorithm>
 #include <cctype>
 #include <cstring>
+#include <random>
 
 class Ising
 {
+private:
+    std::mt19937 gen;
 public:
     int N, nsteps;
     double temp;  
@@ -20,17 +23,16 @@ public:
     double magnetization;
     int step;
     int* config;
+    int seed;
     // Definintions for dump and log outputs
     std::string dump_file_name = "dump.ising_config";
     std::string log_file_name = "log.ising";
     int output_freq;
     std::ofstream fp;
     std::string columns ="id type x y z s";
+
     // Class constructor
-    // Ising(int lattice_size, double boltzmann_temp){
     Ising(std::string input_file_name){
-        // N = lattice_size;
-        // temp = boltzmann_temp;
         Read_input_file(input_file_name);
         Setup_simulation();
     }
@@ -51,7 +53,7 @@ public:
                 input_commands.push_back(current_line);
             }
             input_file.close();
-            // Now Parsing the input commands
+            //  Parsing the input commands as a Key and value pair
             std::string key, value;
 
             for (int j=0; j<input_commands.size(); j++){
@@ -76,6 +78,9 @@ public:
                 else if (key=="output_frequency"){
                     output_freq = std::stoi(value);
                 }
+                else if (key=="seed"){
+                    seed = std::stoi(value);
+                }
 
             }
             input_file.close();
@@ -87,29 +92,36 @@ public:
     }
     void Setup_simulation(){
         config  = new int[N*N];
+        gen.seed(seed);
         fp.open(dump_file_name, std::ios::out);
 
     }
 
     void Initialize(){
         step=0;
+        std::uniform_int_distribution<int>spin_distr(0,1);
         int row,col;
         for ( row=0; row<N; row++){
             for ( col=0; col<N; col++){
-                config[row*N+col] = (rand() % 2 == 0) ? -1 : 1;
+                config[row*N+col] = (spin_distr(gen) == 0) ? -1 : 1;
                 // printf("x= %d y= %d: spin=%d ", i, j, config[i][j]);
             }
         }
     }
     void MC_Move(){ 
+        std::uniform_int_distribution<int> int_distr(0,N-1);
+        std::uniform_real_distribution<double> real_distr(0.0, 1.0);
         int row, col, spin, ry, rx, neigh_sum, cost;
+        double rn;
         for (row=0; row<N; row++){
             for (col=0; col<N; col++){
-                ry=  rand()%N;
-                rx = rand()%N;
+                // ry=  rand()%N;
+                // rx = rand()%N;
+                ry = int_distr(gen);
+                rx = int_distr(gen);
                 spin = config[ry*N+rx];
                 neigh_sum = config[((ry-1+N)%N)*N+rx] + config[N*((ry+1)%N)+rx] + config[ry*N+(rx-1+N)%N] + config[ry*N+(rx+1)%N];
-                double rn = rand()/(double)RAND_MAX;
+                rn = real_distr(gen);
                 cost = 2*spin*neigh_sum;
                 if (cost<0){
                     spin*=-1;
@@ -163,6 +175,7 @@ public:
         fp.flush();
         
     }
+
 };
 
 
@@ -174,7 +187,6 @@ int main(int argn, char* argv[]){
         return 1;
     }
     std::string input_file = argv[1];
-    srand(100);
     double energy, mag;
     Ising*  ising_sim = new Ising(input_file);
     int nsteps = 400000;
