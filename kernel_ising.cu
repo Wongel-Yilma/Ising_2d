9f0841bba8bfe83499ea51c7e131b6b66e97b7f1
Wongel-Yilma <wongel.yilma2016@gmail.com>
2026-04-05 19:23:20 -0600

    Added c++ file and kernel function for GPU execution

---
+++ kernel_ising.cu
@@ -0,0 +1,87 @@
+#include <cuda_runtime.h>
+#include <math.h>
+#include <stdio.h>
+#include <cassert>
+
+__global__ void mcMoveKernel(int *input_config_d, int * output_config_d, double * rand_nums_d, int N, int phase, double temp){
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+    int spin;
+    double rn, ediff;
+    double neigh_sum;
+
+    if (row< N && col <N){
+        if (((phase+row+col)%2==0)){
+            spin = input_config_d[row*N+col];
+            rn  = rand_nums_d[row*N+col];
+            neigh_sum = input_config_d[((row-1+N)%N)*N+col] + 
+                                    input_config_d[N*((row+1)%N)+col] + 
+                                    input_config_d[row*N+(col-1+N)%N] + 
+                                    input_config_d[row*N+(col+1)%N];
+            ediff = 2*spin*neigh_sum;
+            if (ediff<0.0){
+                spin*=-1;
+            }
+            else if (rn<(exp(-ediff/temp))){
+                spin*=-1;
+            }
+            output_config_d[row*N+col]= spin;
+        }
+        output_config_d[row*N+col]= spin;
+    }
+}
+inline cudaError_t checkCuda(cudaError_t result){
+    if (result !=cudaSuccess){
+        fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+        assert(result==cudaSuccess);
+    }
+    return result;
+}
+
+extern "C" void launch_kernel_ising(int * input_config_h, int * output_config_h, double *rand_nums_h, int N, double temp){
+    
+    int threadsPerBlock = 16;
+    int size_config = N*N*sizeof(int);
+    int size_rn = N*N*sizeof(float);
+
+    
+    // Creating device variables
+    int *input_config_d, *output_config_d;
+    double *rand_nums_d;
+
+    // Allocating memory for the device variables
+    checkCuda(cudaMalloc((void **)&input_config_d, size_config));
+    checkCuda(cudaMalloc((void **)&output_config_d, size_config));
+    checkCuda(cudaMalloc((void **)&rand_nums_d, size_rn));
+    // Copying data from host to device
+    checkCuda(cudaMemcpy(input_config_d, input_config_h, size_config, cudaMemcpyHostToDevice));
+    checkCuda(cudaMemcpy(rand_nums_d, rand_nums_h, size_rn, cudaMemcpyHostToDevice));
+    
+    // Calculating the block and grid sizes
+    dim3 DimGrid((N+threadsPerBlock-1)/threadsPerBlock,(N+threadsPerBlock-1)/threadsPerBlock,1 );
+    dim3 DimBlock(threadsPerBlock, threadsPerBlock, 1);
+
+    // Executing with 2 phases --> Checkerboard scheme
+    int phase;
+    // First phase execution
+    phase = 0;
+
+
+    mcMoveKernel<<<DimGrid, DimBlock>>>(input_config_d, output_config_d, rand_nums_d, N, phase, temp);
+    cudaDeviceSynchronize();
+    // Copying the result of the first phase to perform the second phase;
+    checkCuda(cudaMemcpy(input_config_d, output_config_d,size_config , cudaMemcpyDeviceToDevice));
+    // Second phase execution
+    phase = 1;
+    mcMoveKernel<<<DimGrid, DimBlock>>>(input_config_d, output_config_d, rand_nums_d, N, phase, temp);
+    cudaDeviceSynchronize();
+    checkCuda(cudaMemcpy(output_config_h, output_config_d, size_config, cudaMemcpyDeviceToHost));
+
+    int sum = 0;
+    for (int i=0; i< N*N; i++) sum+= output_config_h[i];
+    printf("Total sum of the output in the cuda file %d\n", sum);
+    cudaFree(input_config_d);
+    cudaFree(output_config_d);
+    cudaFree(rand_nums_d);
+
+}
